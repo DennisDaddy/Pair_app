dc84c33835b3aec106a2cd9a8f8a0fad5a73ece2
DennisDaddy <softteez95@gmail.com>
2016-06-14 12:47:19 +0300

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'staticpages/home'
+
+  get 'staticpages/about'
+
+  get 'staticpages/help'
+
+  get 'staticpages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
