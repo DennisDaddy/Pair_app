9630f74f3fde96a3559a727a60f84158d51daac0
DennisDaddy <softteez95@gmail.com>
2016-06-20 16:36:58 +0300

    test links

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+  	get root_path
+  	assert_select "a[href=?]", about_path
+  	assert_select "a[href=?]", contact_path
+  	assert_select "a[href=?]", help_path
+  
+
+
+end
+end
