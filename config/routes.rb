28e825401511b9caafe9cb51439fa4109c813c0d
DennisDaddy <softteez95@gmail.com>
2016-06-20 15:38:41 +0300

    add page titles

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  
   get 'staticpages/home'
 
-  get 'staticpages/about'
+  get   'staticpages/about'
 
   get  'staticpages/help'
 
   get 'staticpages/contact'
+
+  
   
 end
--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -4,20 +4,24 @@ class StaticpagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get staticpages_home_url
     assert_response :success
+    assert_select "title", "Home |Pair App"
   end
 
   test "should get about" do
     get staticpages_about_url
     assert_response :success
+     assert_select "title", "About |Pair App"
   end
 
   test "should get help" do
     get staticpages_help_url
     assert_response :success
+     assert_select "title", "Help |Pair App"
   end
 
   test "should get contact" do
     get staticpages_contact_url
     assert_response :success
+     assert_select "title", "Contact |Pair App"
   end
 end
